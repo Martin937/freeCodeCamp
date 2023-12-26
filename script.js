7b1895629d28be764d4a8a12da2055b892fdee6c
Martin937 <Martin937@yandex.ru>
2023-12-26 20:01:07 +0300

    93 step

--- script.js
+++ script.js
@@ -99,7 +99,12 @@ function buyHealth() {
 }
 function buyWeapon() {
   if (gold >= 30) {
-    gold -= 30
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
   }
 }
 function fightSlime() {
